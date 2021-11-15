cdfe7740f54329694bd01eed3ef5f594dc68f0b0
mcieciora <michalcieciora96@gmail.com>
2021-11-15 18:53:32 +0100

    commit

--- test_calc_multiply.py
+++ test_calc_multiply.py
@@ -6,7 +6,7 @@ def test_calc_multiply__multiply_positives():
 
 
 def test_calc_multiply__multiply_negatives():
-    assert multiply(-3, -2) == 6, 'Return value shall be 6'
+    assert multiply(-3, -2) == 'fail', 'Return value shall be 6'
 
 
 def test_calc_multiply__multiply_mixed():
